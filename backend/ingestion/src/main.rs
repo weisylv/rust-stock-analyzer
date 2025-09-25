7ce8fda77061d49c955ec0f30201c1b4745c00f4
sylvwei <95110371+sylvwei@users.noreply.github.com>
2025-09-24 23:19:44 -0400

    added the rest of the updated files

--- backend/ingestion/src/main.rs
+++ backend/ingestion/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use dotenvy::dotenv;
+use std::env;
+
+#[tokio::main]
+async fn main(){
+    dotenv().ok();
+
+    //Example: load stock API key
+    let api_key = env::var("STOCK_API_KEY").unwrap_or("missing".to_string());
+
+    println!("Ingestiong service starting...");
+    println!("Using STOCK_API_KEY: {}", if api_key == "missing" {"NOT SET"} else {"SET"});
+
+    // TODO: implement fetch from stock API
 }
